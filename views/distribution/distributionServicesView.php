f2d3f8a9f86be1af27e71ad3e6be62f20a2261f3
Walid-Asakak <asakakwalid@gmail.com>
2026-06-23 21:13:19 +0200

    feat(distribution): Update distribution page styles and services view

--- views/distribution/distributionServicesView.php
+++ views/distribution/distributionServicesView.php
@@ -6,7 +6,7 @@
         <h2>Optimisation de la diffusion - Plan Média</h2>
 
         <p>
-            Selon le contenu, vous proposons un accompagnement personnalisé afin de maximiser votre visibilité 
+            Selon le contenu, vous proposons un accompagnement personnalisé afin de maximiser votre visibilité
             sur les médias traditionnels et numériques grâce à diverses moyens d'optimisation.
         </p>
 
@@ -27,7 +27,7 @@
             <h2>Communication digitale</h2>
 
             <ul>
-                <li>Nous pouvons créer une chaîne YouTube, spécifiquement pour la marque.</li>
+                <li>Nous pouvons créer une chaîne YouTube spécifique à la marque.</li>
                 <li>Nous pouvons placer les vidéos sur Vimeo.</li>
                 <li>Communication sur Instagram avec des photos et des capsules vidéos de 15″.</li>
                 <li>Communication sur les réseaux sociaux (Facebook, Twitter, …)</li>
@@ -82,11 +82,115 @@
             <ul>
                 <li>A. Format de 5 à 10 minutes : Nous les diffusons intégralement.</li>
                 <li>
-                    B. Les capsules de 1 min 30 à 3 minutes : Les capsules mettent en évidence la marque pour 
+                    B. Les capsules de 1 min 30 à 3 minutes : Les capsules mettent en évidence la marque pour
                     chacun des spots sur YouTube mais également pour les capsules de la télévision internet.
                     On peut par exemple également ajouter les bandeaux directement sur le site.
                 </li>
             </ul>
+        </div>
+    </section>
+
+    <!-- 4 - TV INTERNET -->
+    <section class="distribution-card">
+        <div class="distribution-image">
+            <img src="public/assets/img/distribution/travel-world.jpg" alt="TV Internet">
+        </div>
+
+        <div class="distribution-content">
+            <h2>Fashion Channel</h2>
+
+            <p>
+                Fashion Channel 80 000 connexions par jour.
+            </p>
+
+            <p>
+                C'est une plateforme VOD Vidéo en demande (l'annonce avec la marque est placée
+                avant les formats longs de 5 à 10 minutes).
+            </p>
+        </div>
+    </section>
+
+    <!-- 5 - TRAVEL CHANNEL -->
+    <section class="distribution-card">
+        <div class="distribution-image">
+            <img src="public/assets/img/distribution/travel-channel.jpg" alt="Travel Channel">
+        </div>
+
+        <div class="distribution-content">
+            <h2>Travel Channel</h2>
+
+            <p>
+                Voyage Channel est une propriété américaine, britannique de télévision par satellite canal, la
+                transmission 24 heures par jour dans 21 langues à travers l'Europe, le Moyen-Orient, Afrique et
+                Asie-Pacifique. Travel Channel touche 94 millions de foyers aux USA et est diffusé dans 130 pays.
+                Avec Travel Channel, y a aussi le site internet et ses utilisateurs qui sont important avec plus
+                de 170 millions de consommateurs chaque mois. Ces sites d'accompagnement complètent la programmation
+                avec la vidéo et les médias sociaux. La chaîne s'appuie sur la puissance de Voyage comme une force de
+                transformation dans nos vies.
+            </p>
+        </div>
+    </section>
+
+    <!-- 6 - TV5MONDE -->
+    <section class="distribution-card">
+        <div class="distribution-image">
+            <img src="public/assets/img/distribution/tv5.jpg" alt="TV5MONDE">
+        </div>
+
+        <div class="distribution-content">
+            <h2>TV5MONDE</h2>
+
+            <p>
+                Première chaîne généraliste mondiale en langue française, TV5MONDE est le deuxième réseau
+                international de télévision diffusé auprès de 215 millions de foyers dans près de 200 pays et
+                territoires. L'ensemble des programmes de TV5MONDE rassemble une audience cumulée hebdomadaire
+                de 55 millions de téléspectateurs uniques. Sous le label TV5MONDE PLUS, la chaîne propose une
+                offre de programmes en ligne à partir de son site Internet
+                <a href="https://www.tv5monde.com" target="_blank">
+                    tv5monde.com
+                </a>
+                qui enregistre 8 millions de visites chaque mois.
+            </p>
+        </div>
+    </section>
+
+    <!-- 7 - FRANCE Ô -->
+    <section class="distribution-card">
+        <div class="distribution-image">
+            <img src="public/assets/img/distribution/france-o.jpg" alt="France Ô">
+        </div>
+
+        <div class="distribution-content">
+            <h2>France Ô</h2>
+
+            <p>
+                France Ô est une chaîne de télévision généraliste communautaire et nationale française de
+                service public, qui exprime la place de la France d'Outre-mer dans le paysage audiovisuel
+                français. Elle s'adresse en priorité aux Français originaires d'Outre-Mer, toutes générations
+                confondues. France Ô vise d'autres Français ou étrangers, originaires d'autres régions du globe
+                par proximité non seulement géographique, mais culturelle et de centres d'intérêts. L'audience
+                serait de 190 000 spectateurs en France et les DOM TOM. La chaîne dispose aussi d'une visibilité
+                internet.
+            </p>
+        </div>
+    </section>
+
+    <!-- 8 - LUXE TV -->
+    <section class="distribution-card">
+        <div class="distribution-image">
+            <img src="public/assets/img/distribution/luxe-tv.jpg" alt="Luxe TV">
+        </div>
+
+        <div class="distribution-content">
+            <h2>Luxe TV</h2>
+
+            <p>
+                LUXE TV est une chaîne de télévision internationale luxembourgeoise exclusivement consacrée
+                au monde du luxe et de l’art de vivre. La priorité a été donnée à la diffusion en HD en raison
+                de la qualité des images reconnue par tous les opérateurs et les téléspectateurs.
+                LUXE TV est captée dans 107 pays dans le monde; plus de 92 millions de foyers ont un accès direct
+                à la chaîne, dont 6 millions en France.
+            </p>
         </div>
     </section>
 
