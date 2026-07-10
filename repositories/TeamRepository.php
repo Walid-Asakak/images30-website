f8928870c7a847aca0db93d062838035798e87a1
Walid-Asakak <asakakwalid@gmail.com>
2026-07-10 14:24:46 +0200

    feat(team): add translations and improve team repository

--- repositories/TeamRepository.php
+++ repositories/TeamRepository.php
@@ -14,28 +14,61 @@ class TeamRepository {
         $this->pdo = $db->getConnexionDb();
     }
 
+    // Retrieves all team members according to the selected language
     public function getAllTeamMembers(): ?array {
         try {
-            $request = $this->pdo->prepare("SELECT * FROM team_members");
-            $request->execute();
-
+            $request = $this->pdo->prepare("
+                SELECT
+                    team_member.id,
+                    team_member.firstname,
+                    team_member.lastname,
+                    team_member.photo_url,
+                    team_member.video_url,
+                    translation.job_title,
+                    translation.description
+                FROM team_members AS team_member
+                INNER JOIN team_member_translations AS translation
+                    ON team_member.id = translation.team_member_id
+                WHERE translation.language = :language
+        ");
+    
+            $request->execute([
+                ':language' => $_SESSION['language'] ?? 'fr'
+            ]);
+    
             return $request->fetchAll(PDO::FETCH_ASSOC);
         }
-
+    
         catch (Exception $e) {
             return [];
         }
     }
 
+    // Retrieves all team members according to the selected language
     public function getTeamMemberById(int $id): ?array {
-        $request = $this->pdo->prepare("SELECT * FROM team_members WHERE id = :id");
-        
+        $request = $this->pdo->prepare("
+            SELECT
+                team_member.id,
+                team_member.firstname,
+                team_member.lastname,
+                team_member.photo_url,
+                team_member.video_url,
+                translation.job_title,
+                translation.description
+            FROM team_members AS team_member
+            INNER JOIN team_member_translations AS translation
+                ON team_member.id = translation.team_member_id
+            WHERE team_member.id = :id
+            AND translation.language = :language
+        ");
+    
         $request->execute([
             ':id' => $id,
+            ':language' => $_SESSION['language'] ?? 'fr'
         ]);
         
         $result = $request->fetch(PDO::FETCH_ASSOC);
-
+    
         return $result ?: null;
     }
 }
